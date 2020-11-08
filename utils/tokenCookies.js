1b3789ed9054bfc0baded953055df962200afda2
Ekenzy-101 <ekeneonyekaba@gmail.com>
2020-11-08 02:05:10 +0100

    Made some changes

--- utils/tokenCookies.js
+++ utils/tokenCookies.js
@@ -2,8 +2,9 @@ module.exports = ({ accessToken, refreshToken }) => {
   const cookieOptions = {
     httpOnly: true,
     secure: true, //for HTTPS only
-    domain: process.env.FRONTEND_ENDPOINT,
-    sameSite: "none",
+    path: "/",
+    // domain: process.env.FRONTEND_ENDPOINT,
+    // sameSite: "none",
   };
   return {
     access: ["access", accessToken, cookieOptions],
