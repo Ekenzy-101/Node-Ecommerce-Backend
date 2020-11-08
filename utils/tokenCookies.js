22ad9b3946724bd5bd5b0fb8800527cb3cc23af2
Ekenzy-101 <ekeneonyekaba@gmail.com>
2020-11-08 03:47:53 +0100

    Made some changes

--- utils/tokenCookies.js
+++ utils/tokenCookies.js
@@ -3,7 +3,7 @@ module.exports = ({ accessToken, refreshToken }) => {
     httpOnly: true,
     secure: true, //for HTTPS only
     domain: process.env.COOKIE_DOMAIN,
-    sameSite: "none",
+    sameSite: true,
   };
   return {
     access: ["access", accessToken, cookieOptions],
